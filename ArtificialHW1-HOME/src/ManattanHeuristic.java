313d39fac5f6fcd08eafc3168b032bb873b40031
diego <diego@192.168.1.100>
2016-11-05 14:45:48 +0100

    Updated with better heuristic funcion

--- ArtificialHW1-HOME/src/ManattanHeuristic.java
+++ ArtificialHW1-HOME/src/ManattanHeuristic.java
@@ -10,15 +10,15 @@ public class ManattanHeuristic implements HeuristicFunction{
 		Position robotPosition = currEnv.getRobotPosition();
 		Position goalPosition = currEnv.getGoalPosition();
 		Position startPosition = currEnv.getStartPosition();
-		return Math.abs(robotPosition.x() - goalPosition.x()) +Math.abs(robotPosition.y() - goalPosition.y());
+		//return Math.abs(robotPosition.x() - goalPosition.x()) +Math.abs(robotPosition.y() - goalPosition.y());
 		
 		//Prefer Straight lines heuristics
-		/*int dx1=robotPosition.x()-goalPosition.x();
+		int dx1=robotPosition.x()-goalPosition.x();
 		int dy1=robotPosition.y()-goalPosition.y();
 		int dx2=startPosition.x()-goalPosition.x();
 		int dy2=startPosition.y()-goalPosition.y();
 		double cross =Math.abs(dx1*dy2-dx2*dy1);
-		return cross*0.001;*/
+		return cross*0.001;
 	}
 
 }
